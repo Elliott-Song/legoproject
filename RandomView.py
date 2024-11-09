04c54e5d232db5c8ad3b53f4dc68585aed1dbe28
Elliott Song <song.elliott2@gmail.com>
2024-11-08 22:53:35 -0500

    refined image generation, real data added

--- RandomView.py
+++ RandomView.py
@@ -54,7 +54,7 @@ def render_image(obj_path, output_path):
     material.node_tree.links.new(texture.outputs['Color'], color_ramp.inputs['Fac'])
     material.node_tree.links.new(color_ramp.outputs['Color'], bump.inputs['Height'])
     material.node_tree.links.new(bump.outputs['Normal'], material.node_tree.nodes['Principled BSDF'].inputs['Normal'])
-    material.node_tree.nodes['Principled BSDF'].inputs['Roughness'].default_value = random.uniform(0.3, 0.7)
+    material.node_tree.nodes['Principled BSDF'].inputs['Roughness'].default_value = random.uniform(0.3, 0.8)
 
     # Assign the material to the plane
     bpy.context.object.data.materials.append(material)
@@ -67,6 +67,10 @@ def render_image(obj_path, output_path):
     obj = bpy.context.selected_objects[0]
     bpy.context.view_layer.objects.active = obj
     bpy.ops.object.shade_smooth()
+
+    # get width and length of object
+    width = obj.dimensions[0]
+    length = obj.dimensions[1]
 
     # Randomize object position
     obj.location = (random.uniform(-3, 3), random.uniform(-3, 3), 0)
@@ -87,6 +91,44 @@ def render_image(obj_path, output_path):
     obj.data.materials.append(material)
     obj.active_material = material
 
+    def add_sphere():
+        # Add sphere around the object
+        bpy.ops.mesh.primitive_uv_sphere_add(radius=(width + 5) * random.uniform(1.5, 2.5) + 55, location=obj.location)
+        sphere = bpy.context.selected_objects[0]
+
+        # Set random material for the sphere
+        material = bpy.data.materials.new(name="SphereMaterial")
+        material.use_nodes = True
+
+        # Use random image form /dust folder
+        image_path = "C:/Users/Elliott/3D Objects/Code/legoproject/dust/" + random.choice(os.listdir("C:/Users/Elliott/3D Objects/Code/legoproject/dust/"))
+        image_texture = material.node_tree.nodes.new('ShaderNodeTexImage')
+        image_texture.image = bpy.data.images.load(image_path)
+
+        # Create a texture coordinate node to control the texture mapping
+        texture_coordinate = material.node_tree.nodes.new('ShaderNodeTexCoord')
+
+        # Create a mapping node to control the texture mapping
+        mapping = material.node_tree.nodes.new('ShaderNodeMapping')
+        mapping.inputs['Scale'].default_value = (random.uniform(0.7, 3), random.uniform(0.7, 3), random.uniform(0.7, 3))
+        mapping.inputs['Rotation'].default_value = (0, 0, radians(random.uniform(0, 360)))
+        mapping.inputs['Location'].default_value = (random.uniform(-100, 100), random.uniform(-100, 100), random.uniform(-100, 100))
+
+        # Connect the nodes
+        material.node_tree.links.new(texture_coordinate.outputs['UV'], mapping.inputs['Vector'])
+        material.node_tree.links.new(mapping.outputs['Vector'], image_texture.inputs['Vector'])
+        material.node_tree.links.new(image_texture.outputs['Color'], material.node_tree.nodes['Principled BSDF'].inputs['Base Color'])
+        material.node_tree.links.new(image_texture.outputs['Color'], material.node_tree.nodes['Principled BSDF'].inputs['Alpha'])
+
+        # Assign the material to the sphere
+        sphere.data.materials.append(material)
+        sphere.active_material = material
+
+    # Add random number of spheres around the object
+    number_of_spheres = random.randint(1, 1)
+    for i in range(number_of_spheres):
+        add_sphere()
+
     # Sun 1
     sun = bpy.data.lights.new(name="Sun", type='SUN')
     sun_obj = bpy.data.objects.new(name="Sun", object_data=sun)
@@ -94,9 +136,9 @@ def render_image(obj_path, output_path):
 
     # Randomize world background color
     bpy.context.scene.world.node_tree.nodes["Background"].inputs[0].default_value = (
-        random.uniform(0.1, 0.3),
-        random.uniform(0.1, 0.3),
-        random.uniform(0.1, 0.3),
+        random.uniform(0.05, 0.3),
+        random.uniform(0.05, 0.3),
+        random.uniform(0.05, 0.3),
         1
     )
     bpy.context.scene.world.node_tree.nodes["Background"].inputs[1].default_value = random.uniform(0.05, 0.6)
@@ -112,7 +154,7 @@ def render_image(obj_path, output_path):
         radians(random.uniform(-90, 90)),
         radians(random.uniform(0, 360))
     )
-    sun_obj.data.energy = random.uniform(0.1, 3)
+    sun_obj.data.energy = random.uniform(0.05, 5)
 
     # Sun 2
     sun2 = bpy.data.lights.new(name="Sun", type='SUN')
@@ -130,7 +172,7 @@ def render_image(obj_path, output_path):
         radians(random.uniform(-90, 90)),
         radians(random.uniform(0, 360))
     )
-    sun_obj2.data.energy = random.uniform(0, 1)
+    sun_obj2.data.energy = random.uniform(0, 2)
 
     number_of_lights = random.randint(1, 5)
     for i in range(number_of_lights):
@@ -144,7 +186,7 @@ def render_image(obj_path, output_path):
             random.uniform(-50, 50),
             random.uniform(18, 100)
         )
-        light.data.energy = random.uniform(100, 6000)
+        light.data.energy = random.uniform(100, 8000)
         light.data.color = (random.uniform(0.6, 1), random.uniform(0.6, 1), random.uniform(0.6, 1))
 
     # Set up the camera
@@ -155,7 +197,7 @@ def render_image(obj_path, output_path):
 
     # Randomize camera position
     def random_camera_position():
-        camera_distance = random.uniform(90, 190)
+        camera_distance = random.uniform(50, 180) + max(width, length)
         camera_angle_theta = random.uniform(0.4, pi/2)
         camera_angle_phi = random.uniform(0, 2*pi)
 
@@ -180,6 +222,15 @@ def render_image(obj_path, output_path):
 
     random_camera_position()
 
+    # randomize focal length
+    cam.data.lens = random.uniform(40, 74) - max(length, width)/7
+
+    # randomize depth of field
+    cam.data.dof.use_dof = random.choice([True, False])
+    if cam.data.dof.use_dof:
+        cam.data.dof.focus_distance = random.uniform(2, 10) + max(length, width)/7
+        cam.data.dof.aperture_fstop = random.uniform(0.6, 2.8)
+
     # Render settings
     bpy.context.scene.render.engine = 'CYCLES'
     bpy.context.scene.render.filepath = output_path
@@ -189,20 +240,40 @@ def render_image(obj_path, output_path):
     bpy.context.scene.render.resolution_x = 244
     bpy.context.scene.render.resolution_y = 244
 
+    # Random denoise settings
+    bpy.context.scene.cycles.use_denoising = random.choice([True, False])
+    if bpy.context.scene.cycles.use_denoising:
+        bpy.context.scene.cycles.denoiser = 'OPENIMAGEDENOISE'
+
+    # Randomize number of samples
+    bpy.context.scene.cycles.samples = random.randint(200, 500)
+
+    # Randomize film exposure
+    bpy.context.scene.view_settings.exposure = random.uniform(-1, 2)
+
+    # Randomize color management look
+    bpy.context.scene.view_settings.look = random.choice(['Low Contrast', 'Medium High Contrast', 'Very High Contrast', 'High Contrast', 'Medium Contrast', 'None'])
+    bpy.context.scene.view_settings.view_transform = random.choice(['Filmic', 'Standard', 'Standard', 'Raw'])
+
     # Render the image
-    bpy.ops.render.render(write_still=True)
+    # bpy.ops.render.render(write_still=True)
 
     print("Render complete. Image saved to:", output_path)
 
 parent_dir = "C:/Users/Elliott/3D Objects/Code/legoproject/parts_obj"
 obj_paths = os.listdir(parent_dir)
 
+starting_index = 25
+batch_size = 200
+
 for obj_path in obj_paths:
     obj_path = os.path.join(parent_dir, obj_path)
-    for i in range(5):
-        output_path = f"C:/Users/Elliott/3D Objects/Code/legoproject/output/{os.path.splitext(os.path.basename(obj_path))[0]}/{[i]}.png"
+    for i in range(starting_index, starting_index + batch_size):
+        output_dir_name = os.path.splitext(os.path.basename(obj_path))[0].replace(" ", "")
+        output_path = f"C:/Users/Elliott/3D Objects/Code/legoproject/data/synthetic{output_dir_name}/{[i]}.png"
         if not os.path.exists(os.path.dirname(output_path)):
             os.makedirs(os.path.dirname(output_path))
-        render_image(obj_path, output_path)
+        if not os.path.exists(output_path):
+            render_image(obj_path, output_path)
         break
     break
